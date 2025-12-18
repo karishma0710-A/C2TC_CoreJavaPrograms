e89dbbe1ff7bf7e965c01afbc243a165db0f45a2
karis <karis@LAPTOP-9GM3KQQ1>
2025-12-18 12:17:25 +0530

    this is my second commit

---
+++ src/com/testing/junit/Testcase_1.java
@@ -0,0 +1,14 @@
+package com.testing.junit;
+
+import org.junit.jupiter.api.Test;
+
+public class Testcase_1 {
+	
+	@Test
+	void display() {
+	System.out.println("Hi this is our First test ");
+	}
+	
+	
+
+}
